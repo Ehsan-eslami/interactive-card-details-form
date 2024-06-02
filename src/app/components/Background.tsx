385b229a1de6681ec6c4c4a4ab73425bd8512a4e
EHSAN\esehs <eslami.ehsan200@gmail.com>
2024-06-02 12:16:50 +0330

    add alt to each card tag

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,8 +1,8 @@
 export default function Background() {
   return (
     <div className="card-abs w-full md:w-[40%] h-96 md:h-full fixed md:left-0 top-0 z-10 ">
-      <img className="hidden md:block h-full" src="/images/bg-main-desktop.png" alt="" />
-      <img className="block md:hidden w-full" src="/images/bg-main-mobile.png" alt="" />
+      <img className="hidden md:block h-full" src="/images/bg-main-desktop.png" alt="desktop background" />
+      <img className="block md:hidden w-full" src="/images/bg-main-mobile.png" alt="Mobile background" />
     </div>
   )
 }
